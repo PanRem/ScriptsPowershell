f89df6dba3f7aed4c26e1731c6177dc13ad1e6aa
PanRem <154988183+PanRem@users.noreply.github.com>
2024-02-03 18:11:13 +0100

    Add files via upload

---
+++ CreateFolderForEachADuser/Dossier_User.ps1
@@ -0,0 +1,35 @@
+#récupération de tous les usernames de l'AD
+$users = Get-ADUser -filter '*' | Select -Property 'SamAccountName'
+$count = 0
+
+#création de dossier pour chaque user
+foreach ($user in $users.SamAccountName)
+{
+	$path = "\\Emplacement\reseau\partage$\$user"
+	#check si existant
+    if(!(Test-Path -Path $path)){
+        $folder = New-Item $path -ItemType Directory
+
+        #on commence par mettre tous les droits
+        $perms =  "domain\$user", 'FullControl', 'ContainerInherit,ObjectInherit', 'None', 'Allow'
+        $AclObj = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $perms
+		#on récupère les droits du dossier pour les modifier
+        $Acl = Get-Acl $folder.FullName
+        $Acl.SetAccessRuleProtection($true,$true)
+        $Acl.SetAccessRule($AclObj)
+
+		#on retire les droits non voulu
+        $precise1 = New-Object System.Security.AccessControl.FileSystemAccessRule("domain\$user", 'ChangePermissions', 'Allow')
+        $precise2 = New-Object System.Security.AccessControl.FileSystemAccessRule("domain\$user", 'Delete', 'Allow')
+        $precise3 = New-Object System.Security.AccessControl.FileSystemAccessRule("domain\$user", 'TakeOwnership', 'Allow')
+        $ACL.RemoveAccessRule($precise1)
+        $ACL.RemoveAccessRule($precise2)
+        $ACL.RemoveAccessRule($precise3)
+		#enfin on édite les droits effectifs du dossier en conséquence
+        Set-Acl -Path $folder.FullName -AclObject $Acl
+        Write-Host "Dossier $user créé"
+        $count++
+    }
+}
+Write-Host "$count dossiers créés"
+Read-Host -Prompt "Press enter to quit..."
